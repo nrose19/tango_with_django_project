b7a36051dc21eac75a413e74a5fe76f57b3e801d
Nicole Cohen <3121217c@student.gla.ac.uk>
2026-02-23 12:21:34 +0000

    continuation of chapter seven

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def about(request):
     return HttpResponse("Rango says here is the about page.")
@@ -35,6 +36,21 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+def add_category(request):
+    form = CategoryForm()
+
+    #if HTTP post
+    if request.method == 'POST': 
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango')
+    else: 
+        print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form})
+    
 
 
 
